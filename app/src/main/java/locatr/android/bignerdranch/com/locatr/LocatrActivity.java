c879f609ca91ad519fb3ea96e60cba7d882e1de5
Mohamed Elmekki <mohamed.elmekki@gmail.com>
2017-08-13 22:34:39 -0400

    Ch31 Pg 559.

---
+++ app/src/main/java/locatr/android/bignerdranch/com/locatr/LocatrActivity.java
@@ -0,0 +1,14 @@
+package locatr.android.bignerdranch.com.locatr;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocatrActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		return LocatrFragment.newInstance();
+	}
+
+}
